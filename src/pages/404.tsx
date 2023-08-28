5bc1a92a528cc67513bac30ff24b86dc7bc0b96a
Kharchuk Illia <kharchukillia@gmail.com>
2023-08-28 11:17:09 +0300

    feat: add custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "@/styles/404.module.scss";
+import { useRouter } from "next/router";
+const Index = () => {
+	const router = useRouter()
+  return (
+    <div className={styles.message}>
+      <p className={styles.message__text}>Oops... 404 page not found</p>
+      <button className={styles.message__button} onClick={() => router.push("/")}>Return to homegage</button>
+    </div>
+  );
+};
+
+export default Index;
